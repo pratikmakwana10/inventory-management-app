70b798ee8c658aa7e833a626e2aa9ef401e5fd62
OK <test.urbanlyfe@gmail.com>
2024-07-24 22:38:01 +0530

    style: add bold text style

---
+++ lib/styles/styles.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles{
+  static TextStyle boldHeading ()=> const TextStyle(fontWeight: FontWeight.bold,color: Colors.black,fontSize: 22);
+}
